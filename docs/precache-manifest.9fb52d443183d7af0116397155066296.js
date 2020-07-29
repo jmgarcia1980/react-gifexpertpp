3949a4c85e1da4e847b5bbce4eb65394620e4cf9
Juan M Garcia <garcia_juanm@hotmail.com>
2020-07-29 18:46:47 -0300

    Docs agregados..

---
+++ docs/precache-manifest.9fb52d443183d7af0116397155066296.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a2223e816fae61582f67a34e6f4bd5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29dbfe97f4d4f4be7424",
+    "url": "/static/css/main.0984077f.chunk.css"
+  },
+  {
+    "revision": "831e6fd3d7ad9db89c7d",
+    "url": "/static/js/2.f3c95073.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f3c95073.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29dbfe97f4d4f4be7424",
+    "url": "/static/js/main.45ee6741.chunk.js"
+  },
+  {
+    "revision": "50a36b93483816401d2d",
+    "url": "/static/js/runtime-main.296f2ffd.js"
+  }
+]);
